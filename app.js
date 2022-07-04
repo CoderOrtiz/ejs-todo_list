61a4a9a51346c4cb4855e486940021b961d106a1
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-07-04 13:12:40 -0400

    Uploading Views Folder and list.ejs File

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+// Line 7 must be before "const app = express();"
+app.use("view engine", "ejs");
+
 app.get("/", function (require, response) {
 
     let today = new Date();
     let currentDay = today.getDay();
+    let day = "";
 
 
     if (currentDay === 6 || currentDay === 0) {
         response.write("<h1>Its the Weekend</h1>")
     } else {
-        response.write("<p>It is not the weekend</p>")
-        response.write("<h1>I have to work today.</h1>")
-        response.send();
-    }
+        // response.write("<p>It is not the weekend</p>")
+        // response.write("<h1>I have to work today.</h1>")
+        // response.send();
 
-    response.send("Hello World");
+        // Instead of writing lines 18 - 20 you can right the code below.
+
+        response.sendFile(__dirname + "/index.html");
+    }
 });
 
 app.listen(3000, function() {
