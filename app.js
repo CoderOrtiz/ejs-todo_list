84ae26699e740c89405442627d04f3da448f7411
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-07-04 12:13:58 -0400

    Updating app.js File

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const app = express();
 app.get("/", function (require, response) {
 
     let today = new Date();
-    if (today.getDay() === )
+    let currentDay = today.getDay();
+
+
+    if (currentDay === 6 || currentDay === 0) {
+        response.write("<h1>Its the Weekend</h1>")
+    } else {
+        response.write("<p>It is not the weekend</p>")
+        response.write("<h1>I have to work today.</h1>")
+        response.send();
+    }
 
     response.send("Hello World");
 });
