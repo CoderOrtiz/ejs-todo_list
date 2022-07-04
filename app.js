bf2246aa56a6d0d7768f509792472896eeac5c76
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-07-04 13:51:29 -0400

    Ran into a Bug

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-// Line 7 must be before "const app = express();"
-app.use("view engine", "ejs");
+// Line 7 must be after "const app = express();"
+app.use('view engine', 'ejs');
 
 app.get("/", function (require, response) {
 
@@ -14,16 +14,13 @@ app.get("/", function (require, response) {
 
 
     if (currentDay === 6 || currentDay === 0) {
-        response.write("<h1>Its the Weekend</h1>")
+        day = "Weekend";
     } else {
-        // response.write("<p>It is not the weekend</p>")
-        // response.write("<h1>I have to work today.</h1>")
-        // response.send();
-
-        // Instead of writing lines 18 - 20 you can right the code below.
-
-        response.sendFile(__dirname + "/index.html");
+        day = "Weekday";
     }
+
+    response.render("list", {kindOfDay: day});
+    
 });
 
 app.listen(3000, function() {
