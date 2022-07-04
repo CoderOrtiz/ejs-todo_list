26a337d1496151ea3f3cef77941fd033874be2ac
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-07-04 15:57:37 -0400

    app.js now determines the day of the week using a switch statement

--- app.js
+++ app.js
@@ -6,17 +6,40 @@ const app = express();
 // Line 7 must be after "const app = express();"
 app.set('view engine', 'ejs');
 
+let day = "";
+
 app.get("/", function (req, res) {
 
-    let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
+    // The getDay() method returns the weekday as a number between 0 and 6.
+    switch (new Date().getDay()) {
+        
+        case 0:
+            day = "Today is Sunday";
+            break;
+        
+        case 1:
+            day = "Today is Monday";
+            break;
+        
+        case 2:
+            day = "Today is Tuesday";
+            break;
+    
+        case 3:
+            day = "Today is Wednesday";
+            break;
 
+        case 4:
+            day = "Today is Thursday";
+            break;
 
-    if (currentDay === 6 || currentDay === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
+        case 5:
+            day = "Today is Friday";
+            break;
+
+        case 6:
+            day = "Today is Satuday";
+            break;
     }
 
     res.render("list", {kindOfDay: day});
